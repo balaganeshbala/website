4a232ba2603b1099d0712f542428dd9c2ae35772
Balaganesh S <balas0011@gmail.com>
2018-09-20 07:57:40 +0530

    Design changes

--- app/main.js
+++ app/main.js
@@ -6,14 +6,20 @@ import Name from './components/Name'
 import Body from './components/Body'
 import Footer from './components/Footer'
 
+require('./main.css');
+
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <Name />
-        <Body />
-        <Footer />
+        <div id="header-div">
+          <Header />
+        </div>
+        <div id="body-div">
+          <Name />
+          <Body />
+          <Footer />
+        </div>
       </div>
     );
   }
